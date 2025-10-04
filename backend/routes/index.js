48f5fa44e2c938f60248a9e969486538141de48e
trinagilmer <manager@poriruaclub.co.nz>
2025-10-04 20:28:42 +1300

    Fix case sensitivity for views path

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("Pages/dashboard", { title: "Dashboard" });
+  res.render("pages/dashboard", { title: "Dashboard" });
 });
 
 module.exports = router;
